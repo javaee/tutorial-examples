01288e06cefba601620360d17f14a5e9f3f8d1d1
David Kral <david.k.kral@oracle.com>
2017-08-30 09:56:27 +0200

    Dmitrys suggestions implemented
    Signed-off-by: David Kral <david.k.kral@oracle.com>

--- web/jsonb/jsonbbasics/src/main/java/javaeetutorial/jsonbmodel/JsonbBean.java
+++ web/jsonb/jsonbbasics/src/main/java/javaeetutorial/jsonbmodel/JsonbBean.java
@@ -18,9 +18,12 @@ import javax.json.bind.Jsonb;
 import javax.json.bind.JsonbBuilder;
 import javax.json.bind.JsonbConfig;
 
-/* This class manages the data from a JSF form, creates 
- * Object and serialize this object to JSON. This class
-  * also deserialize JSON to object */
+/**
+ * This class manages the data from a JSF form, creates Object and serialize this object to JSON.
+ * This class also deserialize JSON to object.
+ *
+ * @author David Kral
+ */
 @Named
 @SessionScoped
 public class JsonbBean implements Serializable {
@@ -41,70 +44,89 @@ public class JsonbBean implements Serializable {
     private String number2 = "123-555-555";
 
     protected String jsonTextArea = "";
+
+    private Jsonb jsonb;
     
-    public JsonbBean() {}
+    public JsonbBean() {
+
+        /* JsonbConfig and Jsonb instance creation */
+        JsonbConfig config = new JsonbConfig()
+                .withFormatting(true);
+        this.jsonb = JsonbBuilder.create(config);
+    }
     
     /* Getters and setters */
     public String getPhoneTypeHome() {
         return PHONE_TYPE_HOME;
     }
+
     public String getPhoneTypeWork() {
         return PHONE_TYPE_WORK;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
     public String getProfession() {
         return profession;
     }
+
     public void setProfession(String profession) {
         this.profession = profession;
     }
+
     public String getPhoneType1() {
         return phoneType1;
     }
+
     public void setPhoneType1(String phoneType1) {
         this.phoneType1 = phoneType1;
     }
+
     public String getNumber1() {
         return number1;
     }
+
     public void setNumber1(String number1) {
         this.number1 = number1;
     }
+
     public String getPhoneType2() {
         return phoneType2;
     }
+
     public void setPhoneType2(String phoneType2) {
         this.phoneType2 = phoneType2;
     }
+
     public String getNumber2() {
         return number2;
     }
+
     public void setNumber2(String number2) {
         this.number2 = number2;
     }
+
     public String getJsonTextArea() {
         return jsonTextArea;
     }
+
     public void setJsonTextArea(String jsonTextArea) {
         this.jsonTextArea = jsonTextArea;
     }
     
-    /* Action method for the form in index.xhtml.
-     * Creates Person object and creates formatted JSON */
+    /**
+     *  Action method for the form in index.xhtml.
+     *  Creates Person object and creates formatted JSON
+     */
     public String createJson() {
         Person person = new Person(this.name, this.profession, new ArrayList<>());
         person.getPhoneNumbers().add(new PhoneNumber(this.phoneType1, this.number1));
         person.getPhoneNumbers().add(new PhoneNumber(this.phoneType2, this.number2));
-
-        /* JSON-B config and Jsonb instance creation */
-        JsonbConfig config = new JsonbConfig()
-                .withFormatting(true);
-        Jsonb jsonb = JsonbBuilder.create(config);
 
         /* Serialization to JSON */
         this.jsonTextArea = jsonb.toJson(person);
@@ -113,14 +135,13 @@ public class JsonbBean implements Serializable {
         return "jsongenerated";
     }
     
-    /* Action method for form in jsongenerated.xhtml.
+    /**
+     * Action method for form in jsongenerated.xhtml.
      * Deserialize JSON from the textarea to Person object
-     * and fills these data to form. */
+     * and fills these data to form.
+     */
     public String parseJson() {
         clearFields();
-
-        /* JSON-B config and Jsonb instance creation */
-        Jsonb jsonb = JsonbBuilder.create(new JsonbConfig());
 
         /* Deserialization of JSON to Object */
         Person person = jsonb.fromJson(this.jsonTextArea, Person.class);
